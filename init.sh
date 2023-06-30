9cdf2d892a98667e9b9e7876b805450f7016c65a
Ruk Doe <info@doany.io>
2023-06-30 09:57:48 +0900

    fix

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ mkdir -p koel/music koel/covers koel/search_index koel/db
 curl https://raw.githubusercontent.com/5ym/koel-traefik-swarm/main/koel-compose.yml > koel-compose.yml
 docker compose -f koel-compose.yml up -d db
 docker compose -f koel-compose.yml run --rm koel php artisan koel:init --no-assets
-docker compose down
+docker compose -f koel-compose.yml down
 docker stack deploy --with-registry-auth -c koel-compose.yml koel
